80e255d5e20aded774c488db0a39087466f4ae30
Madness <119778678+SM-Madness301@users.noreply.github.com>
2022-12-12 00:57:25 -0500

    Update and rename plugins/info-creador.js to complementos/info-creador.js

---
+++ complementos/info-creador.js
@@ -0,0 +1,41 @@
+let handler = async (m, { conn, usedPrefix }) => {
+var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
+var document = doc[Math.floor(Math.random() * doc.length)]    
+let text = `
+*—◉ 𝙴𝙻 𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝙼𝙸 𝙲𝚁𝙴𝙰𝙳𝙾𝚁 𝙴𝚂 wa.me/5493873084433*
+
+*—◉ 𝙴𝙻 𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴𝙻 𝙲𝙾𝙻𝙰𝙱𝙾𝚁𝙰𝙳𝙾𝚁 𝙴𝚂 wa.me/524531106422*
+`.trim()   
+let buttonMessage= {
+'document': { url: `https://github.com/SM-Madness301` },
+'mimetype': `application/${document}`,
+'fileName': `「  𝑯𝒆𝒍𝒍𝒐 𝑾𝒐𝒓𝒍𝒅 」`,
+'fileLength': 99999999999999, 
+'pageCount': 200,
+'contextInfo': {
+'forwardingScore': 200,
+'isForwarded': true,
+'externalAdReply': {
+'mediaUrl': 'https://github.com/SM-Madness301',
+'mediaType': 2,
+'previewType': 'pdf',
+'title': 'ᴇʟ ᴍᴇᴊᴏʀ ʙᴏᴛ ᴅᴇ ᴡʜᴀᴛsᴀᴘᴘ⁩',
+'body': wm,
+'thumbnail': imagen1,
+'sourceUrl': 'https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA' }},
+'caption': text,
+'footer': wm,
+'buttons':[
+{buttonId: `${usedPrefix}menu`, buttonText: {displayText: '👾 𝙼𝙴𝙽𝚄 👾'}, type: 1}, 
+{buttonId: `${usedPrefix}donar`, buttonText: {displayText: '📮 𝙳𝙾𝙽𝙰𝚁 📮'}, type: 1}],
+'headerType': 6 }
+conn.sendMessage(m.chat, buttonMessage, { quoted: m })
+//let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;🎭𝐌𝐚𝐝𝐧ͩ𝐞ͫ𝐬ᷤ𝐬🎭;;;\nFN:🎭𝐌𝐚𝐝𝐧ͩ𝐞ͫ𝐬ᷤ𝐬🎭\nORG:🎭𝐌𝐚𝐝𝐧ͩ𝐞ͫ𝐬ᷤ𝐬🎭\nTITLE:\nitem1.TEL;waid=5493873084433:+54 9 3873 4433\nitem1.X-ABLabel:🎭𝐌𝐚𝐝𝐧ͩ𝐞ͫ𝐬ᷤ𝐬🎭\nX-WA-BIZ-DESCRIPTION:[❗] ᴄᴏɴᴛᴀᴄᴛᴀ ᴀ ᴇsᴛᴇ ɴᴜᴍ ᴘᴀʀᴀ ᴄᴏsᴀs ɪᴍᴘᴏʀᴛᴀɴᴛᴇs.\nX-WA-BIZ-NAME:🎭𝐌𝐚𝐝𝐧ͩ𝐞ͫ𝐬ᷤ𝐬🎭\nEND:VCARD`
+//await conn.sendMessage(m.chat, { contacts: { displayName: '🎭𝐌𝐚𝐝𝐧ͩ𝐞ͫ𝐬ᷤ𝐬🎭', contacts: [{ vcard }] }}, {quoted: m})
+//const data = global.owner.filter(([id, isCreator]) => id && isCreator)
+//await conn.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
+}
+handler.help = ['owner', 'creator']
+handler.tags = ['info']
+handler.command = /^(owner|creator|creador|propietario)$/i
+export default handler
